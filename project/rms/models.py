74eb54ab06038fb52592501ec139a2f23d99e4b5
Rohan Awal <awalrohan8@gmail.com>
2025-01-22 17:00:04 +0545

    completed model creation

--- project/rms/models.py
+++ project/rms/models.py
@@ -1,3 +1,27 @@
 from django.db import models
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
 
 # Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=10)
+
+class Food(models.Model):
+    name = models.CharField(max_length=58)
+    description = models.TextField(null=True)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    price = models.IntegerField()
+
+class Table(models.Model):
+    number = models.CharField(max_length=1)
+    is_available = models.BooleanField(default=False)
+
+class Order(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    table = models.ForeignKey(Table, on_delete=models.CASCADE)
+    quantity = models.IntegerField(default=1)
+    
+class Order_item(models.Model):
+    order = models.ForeignKey(Order,on_delete=models.PROTECT)
+    food = models.ForeignKey(Food, on_delete=models.PROTECT)
